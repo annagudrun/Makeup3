4f417d19d26a80d89f91d428e39f99f402017ee9
heiddisloa <heiddis14@ru.is>
2015-05-13 17:32:11 +0000

    taka ut full name

--- makeup1Solution/makeup1/Models/IdentityModels.cs
+++ makeup1Solution/makeup1/Models/IdentityModels.cs
@@ -6,7 +6,6 @@ namespace makeup1.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public string FullName { get; set; }
         private string ProfilePic { get; set; }
         public string ProfilePhoto
         {
